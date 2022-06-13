fb00c281365de2d90003da96c7f6c16b659bb2cb
ridalgo- <ridalgo-@student.42sp.org.brr>
2022-06-13 15:55:21 +0200

    treating norminette changes on arg=p

--- b_sources/ft_arg_p.c
+++ b_sources/ft_arg_p.c
@@ -6,11 +6,55 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 19:51:33 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/06/12 22:37:28 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/06/13 15:54:03 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../b_includes/ft_printf_b.h"
+
+static int	ft_nullpointer(int len, va_list arg, t_flags *flag)
+{
+	s = "(nil)";
+	flag->width -= ft_strlen(s);
+	if (flag->width > 0)
+		len += flag->width;
+	if (flag->left)
+	{
+		write(1, s, ft_strlen(s));
+		while (flag->width-- > 0)
+			ft_putchar(' ');
+	}
+	else
+	{
+		while (flag->width-- > 0)
+			ft_putchar(' ');
+		write(1, s, ft_strlen(s));
+	}
+	len = len + ft_strlen(s);
+	return (len);
+}
+
+static int	ft_pointerlen(int len, va_list arg, t_flags *flag)
+{
+	flag->width -= ft_countdigits(p, 16) + 2;
+	if (flag->width > 0)
+		len += flag->width;
+	if (flag->left)
+	{
+		write(1, "0x", 2);
+		ft_putbase(p, HEXA_LOWER);
+		while (flag->width-- > 0)
+			ft_putchar(' ');
+	}
+	else
+	{
+		while (flag->width-- > 0)
+			ft_putchar(' ');
+		write(1, "0x", 2);
+		ft_putbase(p, HEXA_LOWER);
+	}
+	len = len + ft_countdigits(p, 16) + 2;
+}
 
 int	ft_arg_p(int len, va_list arg, t_flags *flag)
 {
@@ -19,45 +63,8 @@ int	ft_arg_p(int len, va_list arg, t_flags *flag)
 
 	p = va_arg(arg, unsigned long);
 	if (!p)
-	{
-		s = "(nil)";
-		flag->width -= ft_strlen(s);
-		if (flag->width > 0)
-			len += flag->width;
-		if (flag->left)
-		{
-			write(1, s, ft_strlen(s));
-			while (flag->width-- > 0)
-				ft_putchar(' ');
-		}
-		else
-		{
-			while (flag->width-- > 0)
-				ft_putchar(' ');
-			write(1, s, ft_strlen(s));
-		}
-		len = len + ft_strlen(s);
-	}
+		len = ft_nullpointer(len, arg, flag);
 	else
-	{
-		flag->width -= ft_countdigits(p, 16) + 2;
-		if (flag->width > 0)
-			len += flag->width;
-		if (flag->left)
-		{
-			write(1, "0x", 2);
-			ft_putbase(p, HEXA_LOWER);
-			while (flag->width-- > 0)
-				ft_putchar(' ');
-		}
-		else
-		{
-			while (flag->width-- > 0)
-				ft_putchar(' ');
-			write(1, "0x", 2);
-			ft_putbase(p, HEXA_LOWER);
-		}
-		len = len + ft_countdigits(p, 16) + 2;
-	}
+		len = ft_pointerlen(len, arg, flag);
 	return (len);
 }
