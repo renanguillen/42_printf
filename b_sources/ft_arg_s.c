7a2cca01e7594a1ac8d209ec99b2f037a9babdfb
ridalgo- <ridalgo-@student.42sp.org.brr>
2022-06-15 16:36:49 +0200

    s and udi arguments changes

--- b_sources/ft_arg_s.c
+++ b_sources/ft_arg_s.c
@@ -6,29 +6,34 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 16:43:30 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/06/13 16:49:02 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/06/13 18:36:54 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../b_includes/ft_printf_b.h"
 
+static char	*ft_strtrim(char *str, t_flags *flag)
+{
+	if (!str)
+		str = ft_substr("(nil)", 0, 5);
+	else
+		str = ft_substr(str, 0, ft_strlen(str));
+	if (flag->precision)
+		str = ft_substr(str, 0, flag->precision);
+	return (str);
+}
+
 int	ft_arg_s(int len, va_list arg, t_flags *flag)
 {
-	char		*s;
-	char		*str;
+	char	*str;
 
-	str = va_arg(arg, char *);
-	s = ft_substr(str, 0, ft_strlen(str));
-	if (!s)
-		s = ft_substr("(null)", 0, 6);
-	else if (flag->precision)
-		s = ft_substr(s, 0, flag->precision);
-	flag->width -= ft_strlen(s);
+	str = ft_strtrim(va_arg(arg, char *), flag);
+	flag->width -= ft_strlen(str);
 	if (flag->width > 0)
 			len += flag->width;
 	if (flag->left)
 	{
-		write(1, s, ft_strlen(s));
+		write(1, str, ft_strlen(str));
 		while (flag->width-- > 0)
 			ft_putchar(' ');
 	}
@@ -36,9 +41,9 @@ int	ft_arg_s(int len, va_list arg, t_flags *flag)
 	{
 		while (flag->width-- > 0)
 			ft_putchar(' ');
-		write(1, s, ft_strlen(s));
+		write(1, str, ft_strlen(str));
 	}
-	len = len + ft_strlen(s);
-	ft_freethis(s);
+	len = len + ft_strlen(str);
+	ft_freethis(str);
 	return (len);
 }
--- b_sources/ft_arg_udi.c
+++ b_sources/ft_arg_udi.c
@@ -6,11 +6,41 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 19:50:09 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/06/13 16:50:25 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/06/13 21:35:25 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../b_includes/ft_printf_b.h"
+
+static void	ft_identation_left(int signal, int i, t_flags *flag)
+{
+	if (flag->plus && signal)
+		ft_putchar('+');
+	else if (flag->space && signal)
+		ft_putchar(' ');
+	if (!signal)
+		ft_putchar('-');
+	ft_putbase(i, DECA);
+	while (!flag->zero && flag->width-- > 0)
+		ft_putchar(' ');
+	while (flag->zero && flag->width-- > 0)
+		ft_putchar('0');
+}
+
+static void	ft_identation_right(int signal, int i, t_flags *flag)
+{
+	if (flag->space && signal && !flag->plus)
+		ft_putchar(' ');
+	while (!flag->zero && flag->width-- > 0)
+		ft_putchar(' ');
+	while (flag->zero && flag->width-- > 0)
+		ft_putchar('0');
+	if (flag->plus && signal)
+		ft_putchar('+');
+	else if (!signal)
+		ft_putchar('-');
+	ft_putbase(i, DECA);
+}
 
 int	ft_arg_udi(const char *format, int len, va_list arg, t_flags *flag)
 {
@@ -19,55 +49,28 @@ int	ft_arg_udi(const char *format, int len, va_list arg, t_flags *flag)
 	int			signal;
 
 	signal = 1;
-	if (*format == 'd' || *format == 'i' || *format == 'u')
+	if (*format == 'u')
+		i = va_arg(arg, unsigned int);
+	else
+		i = va_arg(arg, int);
+	if (i < 0)
 	{
-		if (*format == 'u')
-			i = va_arg(arg, unsigned int);
-		else
-			i = va_arg(arg, int);
-		if (i < 0)
-		{
-			signal--;
-			len++;
-			i *= -1;
-		}
-		intlen = ft_countdigits(i, 10);
-		flag->width -= intlen;
-		if (signal && (flag->plus || flag->space))
-			len++;
-		if (i < 0 || flag->plus || flag->space)
-			flag->width--;
-		if (flag->width > 0)
-			len += flag->width;
-		if (flag->left)
-		{
-			if (flag->plus && signal)
-				ft_putchar('+');
-			else if (flag->space && signal)
-				ft_putchar(' ');
-			if (!signal)
-				ft_putchar('-');
-			ft_putbase(i, DECA);
-			while (!flag->zero && flag->width-- > 0)
-				ft_putchar(' ');
-			while (flag->zero && flag->width-- > 0)
-				ft_putchar('0');
-		}
-		else
-		{
-			if (flag->space && signal && !flag->plus)
-				ft_putchar(' ');
-			while (!flag->zero && flag->width-- > 0)
-				ft_putchar(' ');
-			while (flag->zero && flag->width-- > 0)
-				ft_putchar('0');
-			if (flag->plus && signal)
-				ft_putchar('+');
-			else if (!signal)
-				ft_putchar('-');
-			ft_putbase(i, DECA);	
-		}
-		len += intlen;
+		signal--;
+		len++;
+		i *= -1;
 	}
+	intlen = ft_countdigits(i, 10);
+	flag->width -= intlen;
+	if (signal && (flag->plus || flag->space))
+		len++;
+	if (i < 0 || flag->plus || flag->space)
+		flag->width--;
+	if (flag->width > 0)
+		len += flag->width;
+	if (flag->left)
+		ft_identation_left(signal, i, flag);
+	else
+		ft_identation_right(signal, i, flag);
+	len += intlen;
 	return (len);
 }
